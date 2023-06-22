dae154ad19aa51a3baec7d22435f0da8c60b9c16
mhtaldmr <mht.alidemir@hotmail.com>
2023-06-22 10:36:46 +0300

    Server Hub method written

--- SignalRApp/Hubs/UserHub.cs
+++ SignalRApp/Hubs/UserHub.cs
@@ -4,5 +4,12 @@ namespace SignalRApp.Hubs;
 
 public class UserHub : Hub
 {
+    public static int TotalViews { get; set; } = 0;
 
+    public async Task NewWindowLoaded()
+    {
+        TotalViews++;
+        //sending the new state or the changes to all clients who is connected to hub
+        await Clients.All.SendAsync("updateTotalViews", TotalViews);
+    } 
 }
